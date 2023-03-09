ff6570c93bb852290479cefc0a56b8e2f7381264
matheus pessoa <matheusmpessoa17@gmail.com>
2023-03-09 16:10:11 -0300

    Types of variables and conversion of types

---
+++ CsharpDiretoAoPonto/Conversion.cs
@@ -0,0 +1,36 @@
+﻿partial class Conversion
+{
+    static void Main(string[] args)
+    {
+        #region Conversões
+
+        int notaAluno = 10;
+
+        // conversao implicita
+        double notaAlunoDouble = notaAluno;
+
+
+        // conversao explicita
+        int numeroDoubleComoInt = (int) notaAlunoDouble;
+
+
+        // conversao utilizando Convert
+        string notaString = "10";
+        int notaConvert = Convert.ToInt32(notaString);
+
+
+        // conversao utilizando parse
+        int notaParse = int.Parse(notaString);
+
+        if(int.TryParse(notaString, out int notaTryParse))
+        {
+
+        } else {
+            Console.Write("Número inválido");
+        }
+
+        #endregion
+
+        Console.Read();
+    }
+}
---
+++ CsharpDiretoAoPonto/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("Hello, World!");
+
+partial class Program
+{
+    static void Main(string[] args)
+    {
+        #region Tipos de Dados
+
+        int numeroInt = 50;
+        int maiorNumeroInt = int.MaxValue;
+        int menorNumeroInt = int.MinValue;
+
+        long numeroLong = 12131412414;
+        long maiorNumeroLong = long.MaxValue;
+        long menorNumeroLong = long.MinValue;
+
+        decimal numeroDecimal = 10.52m;
+
+        double numeroDouble = 12.3;
+        double menorNumeroDouble = double.MinValue;
+
+        bool verdadeiro = true;
+        bool falso = false;
+
+        var numero = 10;
+
+        string nome = "Matheus Pessoa";
+        char letra = 'M';
+
+        DateTime entradaEmpresa = new DateTime(2023, 1, 1);
+        TimeSpan quantoTempoEmpresa = DateTime.Now - entradaEmpresa;
+
+        #endregion
+
+        Console.Read();
+    }
+}
